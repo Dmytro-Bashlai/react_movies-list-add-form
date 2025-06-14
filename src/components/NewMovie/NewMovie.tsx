37742a08123fa4664a9598e3c7634868c8c8d15f
Dmytro Bashlai <imaxs2016@gmail.com>
2025-06-14 21:36:01 +0300

    Implement movie list with add movie functionality

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,30 +1,131 @@
-import { useState } from 'react';
+import React, { useCallback, useMemo, useState } from 'react';
 import { TextField } from '../TextField';
+import { Movie } from '../../types/Movie';
 
-export const NewMovie = () => {
+type Props = {
+  onAdd: (movieData: Movie) => void;
+};
+
+export const NewMovie: React.FC<Props> = ({ onAdd }) => {
   // Increase the count after successful form submission
   // to reset touched status of all the `Field`s
-  const [count] = useState(0);
+  const [count, setCount] = useState(0);
+  const [movieData, setMovieData] = useState({
+    title: '',
+    description: '',
+    imgUrl: '',
+    imdbUrl: '',
+    imdbId: '',
+  });
+  const [validationErrors, setValidationErrors] = useState({});
+
+  const handleChange = useCallback((name: string, value: string) => {
+    setMovieData(prevData => ({
+      ...prevData,
+      [name]: value,
+    }));
+  }, []);
+
+  const handleValidationChange = useCallback(
+    (fieldName: string, hasError: boolean) => {
+      setValidationErrors(prevErrors => ({
+        ...prevErrors,
+        [fieldName]: hasError,
+      }));
+    },
+    [],
+  );
+
+  const isFormValid = useMemo(() => {
+    const { title, imgUrl, imdbUrl, imdbId } = movieData;
+    const areRequiredFields =
+      title.trim().length > 0 &&
+      imgUrl.trim().length > 0 &&
+      imdbUrl.trim().length > 0 &&
+      imdbId.trim().length > 0;
+    const hasValidationError = Object.values(validationErrors).some(
+      error => error === true,
+    );
+
+    return areRequiredFields && !hasValidationError;
+  }, [movieData, validationErrors]);
+
+  const clearForm = useCallback(() => {
+    setMovieData({
+      title: '',
+      description: '',
+      imgUrl: '',
+      imdbUrl: '',
+      imdbId: '',
+    });
+
+    setValidationErrors({});
+  }, []);
+
+  const handleSubmit = useCallback(
+    (event: React.FormEvent) => {
+      event.preventDefault();
+
+      if (!isFormValid) {
+        return;
+      }
+
+      setCount(preventCount => preventCount + 1);
+      onAdd(movieData);
+      clearForm();
+    },
+    [clearForm, isFormValid, movieData, onAdd],
+  );
 
   return (
-    <form className="NewMovie" key={count}>
+    <form className="NewMovie" key={count} onSubmit={handleSubmit}>
       <h2 className="title">Add a movie</h2>
 
       <TextField
         name="title"
         label="Title"
-        value=""
-        onChange={() => {}}
+        value={movieData.title}
+        onChange={value => handleChange('title', value)}
+        onValidationChange={handleValidationChange}
         required
       />
 
-      <TextField name="description" label="Description" value="" />
+      <TextField
+        name="description"
+        label="Description"
+        value={movieData.description}
+        onChange={value => handleChange('description', value)}
+        onValidationChange={handleValidationChange}
+      />
 
-      <TextField name="imgUrl" label="Image URL" value="" />
+      <TextField
+        name="imgUrl"
+        label="Image URL"
+        value={movieData.imgUrl}
+        onChange={value => handleChange('imgUrl', value)}
+        isUrlField={true}
+        onValidationChange={handleValidationChange}
+        required
+      />
 
-      <TextField name="imdbUrl" label="Imdb URL" value="" />
+      <TextField
+        name="imdbUrl"
+        label="Imdb URL"
+        value={movieData.imdbUrl}
+        onChange={value => handleChange('imdbUrl', value)}
+        isUrlField={true}
+        onValidationChange={handleValidationChange}
+        required
+      />
 
-      <TextField name="imdbId" label="Imdb ID" value="" />
+      <TextField
+        name="imdbId"
+        label="Imdb ID"
+        value={movieData.imdbId}
+        onChange={value => handleChange('imdbId', value)}
+        onValidationChange={handleValidationChange}
+        required
+      />
 
       <div className="field is-grouped">
         <div className="control">
@@ -32,6 +133,7 @@ export const NewMovie = () => {
             type="submit"
             data-cy="submit-button"
             className="button is-link"
+            disabled={!isFormValid}
           >
             Add
           </button>
--- src/components/TextField/TextField.tsx
+++ src/components/TextField/TextField.tsx
@@ -8,6 +8,8 @@ type Props = {
   placeholder?: string;
   required?: boolean;
   onChange?: (newValue: string) => void;
+  isUrlField?: boolean;
+  onValidationChange: (fieldName: string, hasError: boolean) => void;
 };
 
 function getRandomDigits() {
@@ -21,13 +23,31 @@ export const TextField: React.FC<Props> = ({
   placeholder = `Enter ${label}`,
   required = false,
   onChange = () => {},
+  isUrlField = false,
+  onValidationChange = () => {},
 }) => {
   // generate a unique id once on component load
   const [id] = useState(() => `${name}-${getRandomDigits()}`);
 
   // To show errors only if the field was touched (onBlur)
   const [touched, setTouched] = useState(false);
-  const hasError = touched && required && !value;
+
+  const pattern =
+    // eslint-disable-next-line max-len
+    /^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www\.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@,.\w_]*)#?(?:[,.!/\\\w]*))?)$/;
+  const hasRequiredError = touched && required && !value.trim();
+  const hasUrlError =
+    touched &&
+    value.trim().length > 0 &&
+    isUrlField &&
+    !pattern.test(value.trim());
+  const hasError = hasRequiredError || hasUrlError;
+
+  const handleBlur = () => {
+    setTouched(true);
+    onChange(value.trim().replace(/\s\s+/g, ' '));
+    onValidationChange(name, hasUrlError);
+  };
 
   return (
     <div className="field">
@@ -46,11 +66,16 @@ export const TextField: React.FC<Props> = ({
           placeholder={placeholder}
           value={value}
           onChange={event => onChange(event.target.value)}
-          onBlur={() => setTouched(true)}
+          onBlur={handleBlur}
         />
       </div>
 
-      {hasError && <p className="help is-danger">{`${label} is required`}</p>}
+      {hasRequiredError && (
+        <p className="help is-danger">{`${label} is required`}</p>
+      )}
+      {hasUrlError && (
+        <p className="help is-danger">{`${label} Should be correct`}</p>
+      )}
     </div>
   );
 };
